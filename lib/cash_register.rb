14337fcfdf23ddbdb0b466ed76b696c781d8b0c1
kyle smith <kylemclellan@gmail.com>
2019-03-22 17:54:11 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,10 +12,8 @@ class CashRegister
   def add_item(item, price, quantity = 1)
     @total += price * quantity 
     @last_price = price * quantity
-    i = 0;
-    while i < quantity
+    quantity.times do
       @items << item
-      i += 1
     end
     @last_item = item 
   end
